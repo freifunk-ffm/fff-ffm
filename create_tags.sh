cda6cb4329b9b7cb1dbf9da25d930340e08bcf83
Christof Schulze <christof.schulze@gmx.net>
2014-06-20 00:46:23 +0200

    make create_tags more verbose

--- create_tags.sh
+++ create_tags.sh
@@ -1,9 +1,11 @@
 tagname=$1
-sed s/$2/$1/g < feeds.conf.default > feeds.conf
+set -ix
+sed "s/$2/$1/g" < feeds.conf.default > feeds.conf
 git commit -m "Tagged feeds" feeds.conf
 git push
+
 git tag -a $1 -m "Created by Jenkins build for $1"
-git push origin $1
+git push -u origin $1
 for  feed in $(cat feeds.conf.pushable|awk '{print $2}')
 do
 	cd feeds/$feed 
